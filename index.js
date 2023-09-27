bcae065ba1facffffb5f8fa1eea8660b752724df
Ian July <ianjuly4@gmail.com>
2023-09-27 15:20:03 -0600

    funtions continued

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+}
+saturdayFun()
+
+function mondayWork(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+mondayWork()
+
+function wrapAdjective(flair = "*"){
+    const innerFunction = function (adjective = "|special|"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+    return innerFunction
+}
+wrapAdjective("%")("a dedicated programmer")
+
+
+//function outer(greeting, msg = "It's a fine day to learn") {
+  //const innerFunction = function (name, lang = "Python") {
+    //return `${greeting}, ${name}! ${msg} ${lang}`;
+  //};
+  //return innerFunction;
+//}
